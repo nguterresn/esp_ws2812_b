13f84b8ab726386ab444925f82af327d99e476e7
Nuno Guterres Nogueira <guterresnogueira@gmail.com>
2025-06-01 20:53:45 +0200

    .

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,20 +5,18 @@
 // esp_hal docs:
 // https://docs.espressif.com/projects/rust/esp-hal/1.0.0-beta.0/esp32c6/esp_hal/index.html
 
-use esp32c6_rust_playground::ws2812_b::WS2812B;
-
+use esp_ws2812_b::ws2812_b::WS2812B;
 use esp_hal::{clock::CpuClock, main};
-use esp_println::println;
 
 const GPIO_RGB: u32 = 8;
 
 #[panic_handler]
-fn panic(info: &core::panic::PanicInfo) -> ! {
-    println!(
-        "Panic reason: {} location: {:?}",
-        info.message(),
-        info.location()
-    );
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    // println!(
+    //     "Panic reason: {} location: {:?}",
+    //     info.message(),
+    //     info.location()
+    // );
     loop {}
 }
 
@@ -26,7 +24,6 @@ fn panic(info: &core::panic::PanicInfo) -> ! {
 fn main() -> ! {
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let _peripherals: esp_hal::peripherals::Peripherals = esp_hal::init(config);
-    println!("Start ESP32C6!");
 
     let mut r =
         WS2812B::new(_peripherals.RMT, 80, _peripherals.GPIO8).expect("Failed to create BRG!");
